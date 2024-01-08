dfe2d447cd779f365a59ad2809ec92e53856294c
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-01-08 16:09:39 +0100

    feat: Header

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'HeaderApp',
+}
+</script>
+
+<template>
+
+    <header>
+
+        <div class="container-fluid bg-white">
+            <div class="row">
+                <div class="col-12">
+                    <div class="logo">
+                        <!-- img -->
+                        <h5>Yu-Gi-Oh Api</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+</template>
+
+<style>
+.logo {
+    min-height: 50px;
+    display: flex;
+    align-items: center;
+}
+</style>
